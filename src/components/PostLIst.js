e2c1b88cab157cf966522ecac88172d7241aaf27
Hieu Tran <hmtran.ae@gmail.com>
2019-01-02 14:00:27 -0500

    Set up actions and reducers for api request to jsonplaceholder

--- src/components/PostLIst.js
+++ src/components/PostLIst.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { fetchPosts } from '../actions'
+import { connect } from 'react-redux';
 
 class PostList extends React.Component {
+    componentDidMount() {
+        this.props.fetchPosts();
+    }
+
     render() {
         return <div>PostList</div>
     }
-
 }
 
-export default PostList;
+export default connect(null, { fetchPosts })(PostList);
