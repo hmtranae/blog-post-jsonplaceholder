20be751c9e783b323a19d0fc981cb9f12976f75c
Hieu Tran <hmtran.ae@gmail.com>
2019-01-03 11:17:21 -0500

    solve overloading issue by creating fetchPostsAndUsers that calls on two other action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,13 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 import _ from 'lodash';
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    console.log('About to fetch posts');
+    await dispatch(fetchPosts());
+    const userIds = _.uniq(_.map(getState().posts, 'userId'))
+    userIds.forEach(id => dispatch(fetchUser(id)));
+}
+
 export const fetchPosts = () => async dispatch => {
     const response = await jsonPlaceholder.get('/posts');
 
@@ -10,12 +17,22 @@ export const fetchPosts = () => async dispatch => {
     })
 }
 
-export const fetchUser = (id) => dispatch => _fetchUser(id, dispatch);
-    const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = (id) => async dispatch => {
     const response = await jsonPlaceholder.get(`/users/${id}`);
 
     dispatch({
         type: 'FETCH_USER',
         payload: response.data
     })
-})
+}
+
+// Multiple requests solution with lodash's memoization
+// export const fetchUser = (id) => dispatch => _fetchUser(id, dispatch);
+//     const _fetchUser = _.memoize(async (id, dispatch) => {
+//     const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//     dispatch({
+//         type: 'FETCH_USER',
+//         payload: response.data
+//     })
+// })
